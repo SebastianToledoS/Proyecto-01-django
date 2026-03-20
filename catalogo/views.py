201ad826df94145c336fb9102426cc80a71cd0cf
Sebastián Toledo <sebtoledosarmiento@gmail.com>
2026-03-20 03:03:44 -0300

    actualización formulario

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,39 +1,32 @@
 from django.shortcuts import render, redirect
 from datetime import datetime
-
-## Simula la informacion que obtenemos de una base de datos
-PRODUCTOS_DB = [
-        {'codigo': 'ARR-001', 'nombre': 'Arroz Grado 1', 'precio': 9999999999999, 'stock': 15, 'categoria': 'Abarrotes'},
-        {'codigo': 'ACE-002', 'nombre': 'Aceite de Oliva', 'precio': 5500.00, 'stock':0, 'categoria': 'Abarrotes'},
-        {'codigo': 'DET-003', 'nombre': 'Detergente Líquido', 'precio': 3000.75, 'stock': 8, 'categoria': 'Limpieza'},
-]
+from .models import Producto, Categoria
 
 def lista_productos(request):
     error = None
     if request.method == 'POST':
-        print("Datos recibidos del formulario:", request.POST)  # Debug: Ver qué datos se reciben
         codigo = request.POST.get('codigo').strip().upper();
-        codigo_duplicado = False
-        for producto in PRODUCTOS_DB:
-            if producto['codigo'] == codigo:
-                codigo_duplicado = True
-                break # No hace seguir revisando si ya encontramos un código duplicado
-        if codigo_duplicado:
-            error = f'El código "{codigo}" ya existe. Por favor, ingresa un código único.'
+        if Producto.objects.filter(codigo=codigo).exists():
+            error = f'El código "{codigo}" ya existe. Usa un código diferente.'
         else:
-            nuevo_producto = {
-                'codigo': codigo,
-                'nombre': request.POST.get('nombre'),
-                'precio': float(request.POST.get('precio')),
-                'stock': int(request.POST.get('stock')),
-                'categoria': request.POST.get('categoria'),
-            }
-            PRODUCTOS_DB.append(nuevo_producto)
-            return redirect('lista_productos')  # Redirige para evitar reenvío de formulario
+            categoria_objeto = Categoria.objects.get(id=request.POST.get('categoria_id'))
+            #* SELECT * FROM catalogo_categoria WHERE id = 1
+
+            #* Detras de escena se genera el sql
+            #* INSERT INTO catalogo_producto (codigo, nombre, precio, stock, categoria_id) VALUES (codigo, nombre, precio, stock, categoria_id)
+            Producto.objects.create(
+                codigo=codigo,
+                nombre=request.POST.get('nombre', '').strip(),
+                precio=float(request.POST.get('precio', 0)),
+                stock=int(request.POST.get('stock',0)),
+                categoria=categoria_objeto
+            )
+            return redirect('lista_productos')
 
     contexto = {
         'fecha_hoy': datetime.now(),
-        'productos': PRODUCTOS_DB,
+        'productos': Producto.objects.select_related('categoria').all(),
+        'categorias': Categoria.objects.all(),
         'tienda': 'Minimarket "Sofia"',
         'error': error
     }    
